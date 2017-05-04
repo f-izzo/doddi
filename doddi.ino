49c27295dade6dca247e14c19f0d931043efe43a
Federico Amedeo Izzo <federico.izzo42@gmail.com>
2017-05-04 10:47:16 +0200

    Code refactoring, added shake detection

--- doddi.ino
+++ doddi.ino
@@ -14,11 +14,12 @@
 //#include <SerialFlash.h>
 
 //#define ACCEL
-//#define ACCEL_DEBUG
+#define ACCEL_DEBUG
 #define vib 16
 #define INTERRUPT_PIN  5
 #define PIN            23   // Pin on the Arduino connected to the NeoPixels
 #define NUMPIXELS      8   //Number of NeoPixels attached to the Arduino
+#define SHAKE_THRESH   1500
 
 boolean shake(uint32_t ms, uint32_t timeout, int small);
 
@@ -74,6 +75,7 @@ uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 float final_euler[3];
 float final_ypr[3];
+VectorInt16 aaWorld;
 volatile bool mpuInterrupt = false;
 
 // game global vars
@@ -81,110 +83,8 @@ boolean start = false;
 boolean endgame = false;
 boolean faceChange = false;
 uint8_t activeF = 0;
+boolean shaken = false;
 face F[12] = {0};
-
-void maingame() {
-  /* Game start */
-  Serial.print("Waiting for start... ");
-  Serial.println(start);
-  start = shake(50, 150, 1);
-  if(!start) return;
-
-  Serial.println("START TO PLAY!!                SOUND FEEDBACK-->intro story");
-  Serial.println("Start playing");
-  playSdWav1.play("start1.wav");
-  delay(50); // wait for library to parse WAV info
-  setAllPixels(purple);
-  delay(500); //Delay to make game slower
-
-  // Set faces color to corresponding resources
-  for (int i = 0; i < NUMPIXELS; i++) {
-    if (F[i].isActive == 0) {
-      pixels.setPixelColor(i, F[i].color);
-      pixels.show();
-      delay(50);
-    }
-  }
-  
-  //NOTE nonsense
-  /*boolean endgame = shake(50, 120, 1);
-  while (endgame) {
-    Serial.print("End Game:           enter animation");
-    Serial.println(endgame);
-    Serial.println("Starting minigames ");
-    playSdWav1.play("start1.wav");   //starting minigame sound
-    delay(50); // wait for library to parse WAV info
-    // "animation"
-    for (int i = 0; i < 3; i++) {
-      setAllPixels(purple);
-      setAllPixels(off);
-    }
-    game();
-    endgame = shake(50, 170, 1);   //coment this out to stay inside the loop
-    //minigame sound play
-    Serial.println("End playing");
-    //playSdWav1.play("win.wav");
-    //delay(50); // wait for library to parse WAV info
-  }*/
-  Serial.print("Face: ");
-  Serial.print(activeF + 1);
-  Serial.print("    ");
-  Serial.print("Shake: ");
-  boolean activ = shake(50, 80, 0);
-  Serial.print(activ);
-  Serial.println("    ");
-  // Face highlighting
-  if (F[activeF].isActive == 0) {
-    // simple blink
-    pixels.setPixelColor(activeF, F[activeF].color);
-    pixels.show();
-    delay(100);
-    pixels.setPixelColor(activeF, off);
-    pixels.show();
-    Serial.print("resource = ");
-    Serial.print(F[activeF].resID);
-    //TODO: print resource status from color intensity
-  }
-  //vibration for choosing
-  if (faceChange != activeF)
-  {
-    Serial.println("the face has changed");
-    faceChange = activeF;
-    digitalWrite(vib, HIGH);
-    delay(150);
-    digitalWrite(vib, LOW);
-  }
-  //Face activation sequence
-  while (activ) {
-    //tell me what face was activated
-    Serial.print("Face nr ");
-    Serial.print(activeF + 1);
-    Serial.println(" has been activated");
-    //change color of the activated face to purple
-    pixels.setPixelColor(activeF, purple);
-    pixels.show();
-    F[activeF].isActive = 1;
-    //face activation sound
-    switch(F[activeF].resID)
-    {
-      case 0:
-        playSdWav1.play("s1.wav");
-        break;
-      case 1:
-        playSdWav1.play("s2.wav");
-        break;
-      case 2:
-        playSdWav1.play("s3.wav");
-        break;
-      case 3:
-        playSdWav1.play("s4.wav");
-        break;
-    }
-    delay(50); // wait for library to parse WAV info
-    activ = shake(50, 100, 0);
-    //delay(1000);
-  }
-}
 
 void arrayInit() {
   // Initialize faces array
@@ -214,6 +114,150 @@ void arrayInit() {
   }
 }
 
+void maingame() {
+  /* Game start */
+  Serial.print("Waiting for start... ");
+  Serial.println(start);
+  start = shake(50, 150, 1);
+  if(!start) return;
+
+  Serial.println("START TO PLAY!!                SOUND FEEDBACK-->intro story");
+  Serial.println("Start playing");
+  playSdWav1.play("start1.wav");
+  delay(50); // wait for library to parse WAV info
+  setAllPixels(purple);
+  delay(500); //Delay to make game slower
+
+  // Turn on all faces with colors
+  // Set faces color to corresponding resources
+  for (int i = 0; i < NUMPIXELS; i++) {
+    if (F[i].isActive == 0) {
+      pixels.setPixelColor(i, F[i].color);
+      pixels.show();
+      delay(50);
+    }
+  }
+  shaken = shake(50, 150, 1);
+  if(shaken) game();
+  //NOTE nonsense
+  // Game ending condition?
+  /*
+  boolean endgame = shake(50, 120, 1);
+  while (!endgame) {
+    Serial.println("Starting minigames ");
+    playSdWav1.play("start1.wav");   //starting minigame sound
+    delay(50); // wait for library to parse WAV info
+    // "animation"
+    for (int i = 0; i < 3; i++) {
+      setAllPixels(purple);
+      setAllPixels(off);
+    }    
+    //endgame = shake(50, 170, 1);   //coment this out to stay inside the loop
+    //minigame sound play
+    //Serial.println("End playing");
+    //playSdWav1.play("win.wav");
+    //delay(50); // wait for library to parse WAV info
+  }*/
+  //Serial.print("Face: ");
+  //Serial.print(activeF);
+  //Serial.print("    ");
+  //Serial.print("Shake: ");
+  //shaken = shake(50, 80, 0);
+  //Serial.print(shaken);
+  //Serial.println("    ");
+  // Face highlighting
+  if (F[activeF].isActive == 0) {
+    // simple blink
+    pixels.setPixelColor(activeF, F[activeF].color);
+    pixels.show();
+    delay(100);
+    pixels.setPixelColor(activeF, off);
+    pixels.show();
+    Serial.print("resource = ");
+    Serial.print(F[activeF].resID);
+    //TODO: print resource status from color intensity
+  }
+  //vibration for choosing
+  if (faceChange != activeF)
+  {
+    Serial.println("the face has changed");
+    faceChange = activeF;
+    digitalWrite(vib, HIGH);
+    delay(150);
+    digitalWrite(vib, LOW);
+  }
+  //Face activation sequence
+  while (shaken) {
+    //tell me what face was activated
+    Serial.print("Face nr ");
+    Serial.print(activeF + 1);
+    Serial.println(" has been activated");
+    //change color of the activated face to purple
+    pixels.setPixelColor(activeF, purple);
+    pixels.show();
+    F[activeF].isActive = 1;
+    //face activation sound
+    switch(F[activeF].resID)
+    {
+      case 0:
+        playSdWav1.play("s1.wav");
+        break;
+      case 1:
+        playSdWav1.play("s2.wav");
+        break;
+      case 2:
+        playSdWav1.play("s3.wav");
+        break;
+      case 3:
+        playSdWav1.play("s4.wav");
+        break;
+    }
+    delay(50); // wait for library to parse WAV info
+    shaken = shake(50, 100, 0);
+    //delay(1000);
+  }
+}
+void game(){
+  setAllPixels(off);
+  boolean facesLeft = true;
+  int x = 0;
+  while (facesLeft) {
+    //get the upward face
+    Serial.print("Face active: ");
+    Serial.println(activeF);
+    //check if there are not active faces left
+    for (int i = 0; i < 12; i++) {
+      x = x + F[i].resPresent;
+    }
+    if (x == 12) {
+      facesLeft = false;
+    }
+    else {
+      facesLeft = true;
+      x = 0;
+    }
+    //Highlight face
+    if (F[activeF].resPresent == 0) {
+      uint32_t oldcolor = pixels.getPixelColor(activeF);
+      int oldr = splitColor(oldcolor, 'r');
+      int oldg = splitColor(oldcolor, 'g');
+      int oldb = splitColor(oldcolor, 'b');
+      pixels.setPixelColor(activeF, pixels.Color((oldr + 50), (oldg + 50), (oldb + 50)));
+      pixels.show();
+      F[activeF].resPresent = 1;
+    }
+  }
+  //zeroing faces
+  for (int i = 0; i < 12; i++) {
+    F[i].resPresent = 0;
+  }
+  facesLeft = true;
+  //minigame sound play
+  Serial.println("End playing");
+  playSdWav1.play("win.wav");
+  delay(50); // wait for library to parse WAV info
+}
+
 void setAllPixels(uint32_t color) {
   for (int i = 0; i < NUMPIXELS; i++) {
     pixels.setPixelColor(i, color);
@@ -233,6 +277,94 @@ uint8_t splitColor ( uint32_t c, char value ) {
 
 void dmpDataReady() {
     mpuInterrupt = true;
+}
+
+void setup() {
+  /* Hardware initialization */
+  pinMode(vib, OUTPUT);
+  pinMode(INTERRUPT_PIN, INPUT);
+  pixels.begin();
+  pixels.show();  // turn off all the pixels
+  Serial.begin(9600);
+  SPI.begin();
+  while(!(SD.begin(10))) {
+    Serial.println("Unable to access the SD card");
+    delay(500);
+  }
+  Wire.begin();
+  Wire.setClock(400000);
+  AudioMemory(100);
+  accelinit();
+  /* Data structure initialization */
+  arrayInit();
+}
+
+void loop() {
+  // The main loop is dedicated to the accelerometer
+  VectorInt16 aa;         //accel sensor measurements
+  VectorInt16 aaReal;     // [x, y, z]    gravity-free accel sensor measurements
+  VectorFloat gravity;    //gravity vector
+  Quaternion q;
+  float euler[3];
+  float ypr[3];
+  if (!dmpReady) return;
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // The game code is executed while the accelerometer is not working
+        maingame();
+    }
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+    fifoCount = mpu.getFIFOCount();
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+    // otherwise, check for DMP data ready interrupt
+    } else if (mpuIntStatus & 0x02) {
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        fifoCount -= packetSize;
+        /* Orientation reading */
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetEuler(euler, &q);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        /* Acceleration reading */
+        mpu.dmpGetAccel(&aa, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+        mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+        for(int i=0; i<3; i++)
+        {
+          final_euler[i] = euler[i] * 180/M_PI;
+          final_ypr[i] = ypr[i] * 180/M_PI;
+        }
+        #ifdef ACCEL_DEBUG
+        Serial.print("ypr\t");
+        Serial.print(ypr[0] * 180/M_PI);
+        Serial.print("\t");
+        Serial.print(ypr[1] * 180/M_PI);
+        Serial.print("\t");
+        Serial.println(ypr[2] * 180/M_PI);
+        Serial.print("euler z  ");
+        Serial.println(euler[2] * 180/M_PI);
+        Serial.print("aworld\t");
+        Serial.print(aaWorld.x);
+        Serial.print("\t");
+        Serial.print(aaWorld.y);
+        Serial.print("\t");
+        Serial.println(aaWorld.z);
+        #endif
+        int newF = getFace();
+        if(newF != activeF) {
+          activeF = newF;
+          Serial.print("Active face: ");
+          Serial.println(activeF);
+        }
+        shaken = getShake();
+        if(shaken) printf("Shake: true");
+    }
 }
 
 void accelinit() {
@@ -262,81 +394,6 @@ void accelinit() {
     }
 }
 
-void setup() {
-  /* Hardware initialization */
-  pinMode(vib, OUTPUT);
-  pinMode(INTERRUPT_PIN, INPUT);
-  pixels.begin();
-  pixels.show();  // turn off all the pixels
-  Serial.begin(9600);
-  SPI.begin();
-  while(!(SD.begin(10))) {
-    Serial.println("Unable to access the SD card");
-    delay(500);
-  }
-  Wire.begin();
-  Wire.setClock(400000);
-  AudioMemory(100);
-  accelinit();
-  /* Data structure initialization */
-  arrayInit();
-  /* Register and start threads */
-  //threads.setSliceMillis(200);
-  //threads.addThread(maingame);
-}
-void loop() {
-  // The main loop is dedicated to the accelerometer
-  VectorInt16 aa;         //accel sensor measurements
-  VectorFloat gravity;    //gravity vector
-  Quaternion q;
-  float euler[3];
-  float ypr[3];
-  if (!dmpReady) return;
-    while (!mpuInterrupt && fifoCount < packetSize) {
-        // The game code is executed while the accelerometer is not working
-        maingame();
-    }
-    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
-    fifoCount = mpu.getFIFOCount();
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-        // reset so we can continue cleanly
-        mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
-    // otherwise, check for DMP data ready interrupt
-    } else if (mpuIntStatus & 0x02) {
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-        fifoCount -= packetSize;
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetEuler(euler, &q);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        for(int i=0; i<3; i++)
-        {
-          final_euler[i] = euler[i] * 180/M_PI;
-          final_ypr[i] = ypr[i] * 180/M_PI;
-        }
-        #ifdef ACCEL_DEBUG
-        Serial.print("ypr\t");
-        Serial.print(ypr[0] * 180/M_PI);
-        Serial.print("\t");
-        Serial.print(ypr[1] * 180/M_PI);
-        Serial.print("\t");
-        Serial.println(ypr[2] * 180/M_PI);
-        Serial.print("euler z  ");
-        Serial.println(euler[2] * 180/M_PI);
-        #endif
-        int newF = getFace();
-        if(newF != activeF) {
-          activeF = newF;
-          Serial.print("Active face: ");
-          Serial.println(activeF);
-        }
-    }
-}
-
 int getFace() {
   int d, iMin = 0, dMin = 999999;
   float fY, fZ, dY, dZ;
@@ -356,11 +413,19 @@ int getFace() {
   }
   if((iMin == 0) || (iMin == 11)) { //Top and bottom face disambiguation
     int eulerZ = (final_euler[2] > 0) ? final_euler[2] : -final_euler[2];
-    iMin = (final_euler[2] > 160) ? 11 : 0;
+    iMin = (eulerZ > 160) ? 11 : 0;
   }
   return iMin; // Index of closest matching face*/
 }
 
+boolean getShake() {
+  int accelx = (aaWorld.x > 0) ? aaWorld.x : -aaWorld.x;
+  int accely = (aaWorld.y > 0) ? aaWorld.y : -aaWorld.y;
+  int accelz = (aaWorld.z > 0) ? aaWorld.z : -aaWorld.z;
+  if(accelx > SHAKE_THRESH || accely > SHAKE_THRESH || accelz > SHAKE_THRESH)
+    return true;
+  else return false;
+}
 //shake detection
 #ifdef ACCEL
 boolean shake(uint32_t ms, uint32_t timeout, int small) {
@@ -415,44 +480,3 @@ boolean shake(uint32_t ms, uint32_t timeout, int small) {
   else return false;
 }
 #endif
-
-void game(){
-  setAllPixels(off);
-  boolean facesLeft = true;
-  int x = 0;
-  while (facesLeft) {
-    //get the upward face
-    Serial.print("Face active: ");
-    Serial.println(activeF);
-    //check if there are not active faces left
-    for (int i = 0; i < 12; i++) {
-      x = x + F[i].resPresent;
-    }
-    if (x == 12) {
-      facesLeft = false;
-    }
-    else {
-      facesLeft = true;
-      x = 0;
-    }
-    //Highlight face
-    if (F[activeF].resPresent == 0) {
-      uint32_t oldcolor = pixels.getPixelColor(activeF);
-      int oldr = splitColor(oldcolor, 'r');
-      int oldg = splitColor(oldcolor, 'g');
-      int oldb = splitColor(oldcolor, 'b');
-      pixels.setPixelColor(activeF, pixels.Color((oldr + 50), (oldg + 50), (oldb + 50)));
-      pixels.show();
-      F[activeF].resPresent = 1;
-    }
-  }
-  //zeroing faces
-  for (int i = 0; i < 12; i++) {
-    F[i].resPresent = 0;
-  }
-  facesLeft = true;
-  //minigame sound play
-  Serial.println("End playing");
-  playSdWav1.play("win.wav");
-  delay(50); // wait for library to parse WAV info
-}
